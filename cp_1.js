070204cbb11448ee13ac7e951fd58875766ee90d
Kenny Nguyen <kennyn@usf.edu>
2026-03-15 12:07:07 -0400

    started js file

---
+++ cp_1.js
@@ -0,0 +1,4 @@
+const form = document.getElementById('feedback-form');
+const display = document.getElementById('feedback-display');
+const tooltip = document.getElementById('tooltip-box');
+const errorMsg = document.getElementById('error-msg');
